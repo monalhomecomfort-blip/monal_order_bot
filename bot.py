be959107843e67788054e313c03bc5e8bc6f57df
monalhomecomfort-blip <monal.home.comfort@gmail.com>
2026-01-16 23:39:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1013,6 +1013,8 @@ async def pay_full(call: types.CallbackQuery):
             "paidAmount": total,
             "dueAmount": 0,
             "paymentLabel": "100% оплата (bot)"
+
+            "userChatId": call.from_user.id
         },
         timeout=10
     )
@@ -1332,6 +1334,7 @@ if __name__ == "__main__":
     app.on_startup.append(on_startup)
 
     web.run_app(app, host="0.0.0.0", port=int(os.getenv("PORT", "8080")))
+
 
 
 
